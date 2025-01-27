b25db24d2c3f228815cda5c545a11498806bc235
dimitrieskam <marija.dimitrieska@students.finki.ukim.mk>
2025-01-28 00:16:05 +0100

    September exam

--- Laboratoriska4B/src/main/java/mk/finki/ukim/mk/lab/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-//package mk.finki.ukim.mk.lab.service;
-//
-//import org.springframework.security.core.userdetails.User;
-//
-//public interface AuthService {
-//    User login(String username, String password);
-//
-//
-//}
